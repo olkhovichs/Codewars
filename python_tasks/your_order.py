bd49d0334002f2892e1cadc3f5243785066bda97
olkhovichs <olkhovichs@icloud.com>
2022-09-07 21:27:45 +0300

    added order word task

---
+++ python_tasks/your_order.py
@@ -0,0 +1,13 @@
+import re
+
+def order(sentence):
+    if not sentence:
+        return sentence
+    else:
+        sent_list = sentence.split(' ')
+        numbers = re.findall('[1-9]', sentence)
+        result = list(dict(sorted((dict(zip(numbers, sent_list))).items())).values())
+        return ' '.join(result)
+
+
+print(order(""))
